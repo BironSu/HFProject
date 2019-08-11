966bb73f5576e73e3b5736fa573dcc803d885c9c
BironSu <bironsu@pursuit.org>
2019-08-11 16:40:15 -0400

    first commit

---
+++ HFProject/Controller/PeopleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleViewController.swift
+//  HFProject
+//
+//  Created by Biron Su on 8/11/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ HFProject/Controller/PlanetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlanetViewController.swift
+//  HFProject
+//
+//  Created by Biron Su on 8/11/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class PlanetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
